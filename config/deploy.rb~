f541c13eeaa11bf71bb7fb14f85c9db3c1a8a48b
Neil Marion dela Cruz <neilmarion@freelancer.com>
2013-07-29 07:43:10 +0800

    Capistrano version

---
+++ config/deploy.rb~
@@ -0,0 +1,20 @@
+require "bundler/capistrano"
+require 'capistrano/ext/multistage'
+
+server "www.gitlab.neilmarion.com", :web, :app, :db, primary: true
+set :stages, %w(production staging)
+set :default_stage, "staging"
+
+set :repo, "gitlabhq"
+set :deploy_via, :remote_cache
+set :use_sudo, false
+
+set :scm, "git"
+set :repository, "git@github.com:neilmarion/#{repo}.git"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+after "deploy", "deploy:cleanup" # keep only the last 5 releases
+
+
---
+++ config/deploy/production.rb
@@ -0,0 +1,37 @@
+set :user, "git"
+set :branch, "capistrano"
+set :application, "paanouplb"
+set :deploy_to, "/home/#{user}/#{application}"
+
+set :rails_env, "production"
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "restarting nginx"
+    task command, roles: :app, except: {no_release: true} do
+      sudo "service nginx restart" 
+    end 
+  end 
+
+  task :setup_config, roles: :app do
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.yml.postgresql"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end 
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end 
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/#{branch}"
+      puts "Run `git push` to sync changes."
+      exit
+    end 
+  end 
+  before "deploy", "deploy:check_revision"
+end
